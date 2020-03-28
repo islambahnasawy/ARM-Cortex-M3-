e835186f87cf936c643c4d5b0e27db995348c272
Islam El-Bahnasawy <islam.elbahnasawy@gmail.com>
2020-03-28 13:49:28 +0200

    Adding sort of documentation and comments to the code.
    1. Adding STD_Types.h which contains the standard types used in the code, ex.: u8, u16,..etc.
    2. Adding sort of documentation and comments to the STK_interface.h file as a user guide to help user anderstanding and using the code.

---
+++ Library/STD_Types.h
@@ -0,0 +1,22 @@
+/********************************************************/
+/*Author : Islam A. El-Bahnasawy                        */
+/*Date   : 13 March 2020								*/
+/*Version : V01											*/
+/********************************************************/
+
+#define u8  unsigned char
+#define u16 unsigned short int
+#define u32 unsigned long int
+typedef unsigned long long u64;
+
+#define s8  signed char
+#define s16 signed short int
+#define s32 signed long int
+
+#define f32 float
+#define f64 double
+#define f96 long double
+
+typedef unsigned char ERR_Status;
+#define OK 	1
+#define NOK 0
--- SysTick_Timer/STK_interface.h
+++ SysTick_Timer/STK_interface.h
@@ -9,11 +9,83 @@
 
 typedef void (*SysTick_CB_t)(void);
 
-
+/******************************************************************************************
+* function name: SysTick_voidInit
+*
+* parameters:  Input: void
+*              Output: NA
+*              In/out: NA
+*
+* return: ERR_Status: OK, NOK
+* Description: This function is used to initialize the SysTick timer peripheral according
+* 			   to user configurations.
+********************************************************************************************/
 extern ERR_Status SysTick_voidInit(void);
+
+
+/******************************************************************************************
+* function name: SysTick_voidStart
+*
+* parameters:  Input: void
+*              Output: NA
+*              In/out: NA
+*
+* return: ERR_Status: OK, NOK
+* Description: This function is used to start the SysTick timer to count.
+********************************************************************************************/
 extern ERR_Status SysTick_voidStart(void);
+
+
+/******************************************************************************************
+* function name: SysTick_voidStop
+*
+* parameters:  Input: void
+*              Output: NA
+*              In/out: NA
+*
+* return: ERR_Status: OK, NOK
+* Description: This function is used to stop the SysTick timer from counting.
+********************************************************************************************/
 extern ERR_Status SysTick_voidStop(void);
+
+
+/******************************************************************************************
+* function name: SysTick_voidSetTime_us
+*
+* parameters:  Input:
+*                time_us
+*                     type: u32
+*                     Description: this variable carries the time to count in Micro-Seconds.
+*
+*                SysTick_Input_CLK_MHZ
+*                	  type: const u8
+*                     Description: this variable carries the SysTick timer input clock in MHZ.
+*
+*              Output: NA
+*              In/out: NA
+*
+* return: ERR_Status: OK, NOK
+* Description: This function is used to set the time (in Micro-Seconds) that the SysTick timer
+* 			   shall count before firing an interrupt.
+********************************************************************************************/
 extern ERR_Status SysTick_voidSetTime_us(u32 time_us, const u8 SysTick_Input_CLK_MHZ);
+
+
+/******************************************************************************************
+* function name: SysTick_voidSetCallBack
+*
+* parameters:  Input:
+*                CallBack_Func
+*                     type: SysTick_CB_t [A pointer to function]
+*                     Description: this variable carries the address of the callback function
+*                     that will be executed in the SysTick Handler.
+*              Output: NA
+*              In/out: NA
+*
+* return:  ERR_Status: OK, NOK
+* Description: This function is used to set the callback function that will be
+* 			   executed in the SysTick Handler.
+********************************************************************************************/
 extern ERR_Status SysTick_voidSetCallBack(SysTick_CB_t CallBack_Func);
 
 #endif
